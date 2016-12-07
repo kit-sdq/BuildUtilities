06f08110d27b77e5d80b4b6517ae17d293510ac0
Heiko Klare <klare.heiko@gmail.com>
2016-12-07 10:13:52 +0100

    Update nightly deploy script

--- travis-ci/deployNightly.sh
+++ travis-ci/deployNightly.sh
@@ -1,28 +1,31 @@
+# Deploys the project update site to the Vitruv update site repository
+# Parameters:
+# $1: The name of the update site (gets aggregated to the aggregate Vitruv site)
+# $2: The path to the update site project in this repository (e.g. releng/tools.vitruv.updatesite)
+
 echo "Build $TRAVIS_JOB_NUMBER"
 echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
 if [ $TRAVIS_PULL_REQUEST == 'false' ]
 then
     echo "Promoting an untagged build on vitruv-tools.github.io/p2/nightly"
-    cd releng/tools.vitruv.updatesite/target
+    cd $2/target
     git clone https://$GITHUB_DEPLOY_TOKEN@github.com/vitruv-tools/p2.git -b gh-pages --quiet
-    if [ -d p2/nightly/framework ]
+    if [ -d p2/nightly/$1 ]
     then
-        rm -r p2/nightly/framework
+        rm -r p2/nightly/$1
         echo "An old version of the nightly repository has been found and removed"
     fi
     echo "Creating the nighly repository"
     cp -r repository p2/nightly/
 	ls p2/nightly
-    mv p2/nightly/repository p2/nightly/framework
+    mv p2/nightly/repository p2/nightly/$1
     echo "Nightly repository created"
-    ls p2/nightly/framework
+    ls p2/nightly/$1
     cd p2/nightly
     git config user.email "vitruv.tools@gmail.com"
     git config user.name "Vitruv Tools"
-    git remote rm origin
-    git remote add origin https://$GITHUB_DEPLOY_TOKEN@github.com/vitruv-tools/p2.git
     git add -A
-    git commit -m "Promoting a new nightly build for https://github.com/vitruv-tools/Vitruv/commit/$TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+    git commit -m "Promoting a new nightly build"
     git push origin gh-pages
-    echo "Build promoted."
+    echo "Build promoted"
 fi
--- travis-ci/installJamopp.sh
+++ travis-ci/installJamopp.sh
@@ -9,6 +9,10 @@
 #	install: true
 #
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 wget -P /tmp http://update.jamopp.org/trunk/plugins/org.emftext.language.java_1.4.1.v201610152108.jar
-mv /tmp/org.emftext.language.java_1.4.1.v201610152108.jar /tmp/org.emftext.language.java_1.4.1.jar
-mvn install:install-file -Dfile=/tmp/org.emftext.language.java_1.4.1.jar -DgroupId=org.emftext -DartifactId=org.emftext.language.java -Dversion=1.4.1 -Dpackaging=jar
+cd /tmp
+mv org.emftext.language.java_1.4.1.v201610152108.jar org.emftext.language.java_1.4.1.jar
+mvn install:install-file -Dfile=org.emftext.language.java_1.4.1.jar -DgroupId=org.emftext -DartifactId=org.emftext.language.java -Dversion=1.4.1 -Dpackaging=jar
+cd -
