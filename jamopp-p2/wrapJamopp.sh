347ad2be63a0dc6108534297d7abb23fa571f914
Travis-CI <travis-ci>
2017-01-24 21:05:34 +0100

    Add explicit release deployment for JaMoPP wrapper

--- jamopp-p2/wrapJamopp.sh
+++ jamopp-p2/wrapJamopp.sh
@@ -6,4 +6,5 @@ mvn clean verify
 export TRAVIS_PULL_REQUEST=false
 export TRAVIS_BRANCH=master
 export GITHUB_DEPLOY_TOKEN=git
-../travis-ci/deploy.sh jamopp-p2 . final "$1" "$2"
+#../travis-ci/deploy.sh jamopp-p2 . final "$1" "$2"
+../travis-ci/deploy.sh jamopp-p2 . final "$1" "$2" 1.0.0
