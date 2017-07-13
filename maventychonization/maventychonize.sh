47ca21210402e3c58d53b5dbb928b7b5c9a11229
Max Kramer <max.kramer@kit.edu>
2017-07-13 16:24:48 +0200

    finished script for POM-less build
    made script move files and verbose

--- maventychonization/maventychonize.sh
+++ maventychonization/maventychonize.sh
@@ -1,5 +1,5 @@
 # This script can be used to make an Eclipse plug-in project ready to be build with maven tycho.
-# It creates and initializes all pom.xml fiels and an extensions.xml files.
+# It creates and initialize a feature, and update site as well as all pom.xml files.
 #
 # usage: 
 # ------
@@ -11,34 +11,49 @@
 # ------------
 # You need to have
 #    a plug-in project named '<groupIDPrefix>.<plugInName>'
-#    stored in a folder named 'bundles', 
+#       stored in a folder named 'bundles', 
 #    a plug-in project with JUnit tests named '<groupIDPrefix>.<plugInName>.tests'
-#    stored in a folder named 'tests',
+#       stored in a folder named 'tests'.
+# 
+# result:
+# -------
+# By running this script you will obtain
 #    a feature project named '<groupIDPrefix>.<plugInName>.feature'
-#    stored in a folder named 'releng', and
+#        stored in a folder named 'features', 
+#    pom.xml files in the 'bundles' and 'features' folder,
+#        which are required for a POM-less build of the plug-ins and features in subfolders of these folders,
+#    an aggregator pom.xml,
+#        where you list all plug-ins and features for the build,
+#    a parent pom.xml, 
+#        for the main build configuration,
 #    an update site project named '<groupIDPrefix>.<plugInName>.updatesite'
-#    stored in the same folder 'releng'.
+#        stored in the same folder 'releng',
+#    an aggregated update site project named '<groupIDPrefix>.<plugInName>.updatesite.aggregated'
+#        stored in the same folder 'releng', and
 #
 # follow-up:
 # ----------
-# You have to 
-#    change the explicit dependencies to features of other update sites in the
-#       relend/...updatesite.aggregated/updatesite.aggr file.
+# After running the script you still have to 
+#    change the dependencies to features of other update sites in the
+#       releng/...updatesite.aggregated/updatesite.aggr file.
 #
-# If you have more than one Eclipse plug-in to be included in the build or
-# if your build depends on other update sites, then you have to 
-#    add your plug-ins to the 
-#       releng/...aggregator/pom.xml file and 
-#    add the update sites to the
-#       releng/...parent/pom.xml file, where we already put TODO comments for you.
+# If you have more than one Eclipse plug-in to be included in the build, then you have to
+#    add these plug-ins to the 
+#       releng/...aggregator/pom.xml file.
+#
+# If your build depends on other update sites, then you have to 
+#    add these update sites to the
+#       releng/...parent/pom.xml file.
 #
 # Furthermore, you may want to 
 #    add a description, copyright, and license text to the 
 #        features/.../feature.xml file and 
 #    add a category description for the update site to the
-#        releng/...updatesite/category.xml file, where we also already put TODO comments for you.
+#        releng/...updatesite/category.xml file.
 #
-#####################################################################
+# At all these locations in the files we have already put TODO comments for you.
+#
+#########################################################################################
 # input:
 GROUPIDPREFIX=$1;
 PLUGINNAME=$2;
@@ -68,54 +83,55 @@ UPDATESITEAGGRFOLDER=$UPDATESITEFOLDER.aggregated;
 UPDATESITEAGGRPROJECT=$UPDATESITEAGGRFOLDER/.project;
 UPDATESITEAGGR=$UPDATESITEAGGRFOLDER/updatesite.aggr;
 UPDATESITEAGGRPOM=$UPDATESITEAGGRFOLDER/pom.xml
-#####################################################################
+#########################################################################################
 # bundles/
-cp bundle_TEMPLATE.pom $BUNDLESPOM;
+mv -v bundle_TEMPLATE.pom $BUNDLESPOM;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $BUNDLESPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $BUNDLESPOM;
 # features/
-mkdir -P $FEATUREFOLDER;
-cp feature_TEMPLATE.project $FEATUREPROJECT;
+mkdir -v -p $FEATUREFOLDER;
+mv -v feature_TEMPLATE.project $FEATUREPROJECT;
 sed -i 's/theGroupID/'$GROUPID'/g' $FEATUREPROJECT;
-cp feature_build_TEMPLATE.properties $FEATUREBUILDPROPS;
-cp feature_TEMPLATE.xml $FEATUREXML;
+mv -v feature_build_TEMPLATE.properties $FEATUREBUILDPROPS;
+mv -v feature_TEMPLATE.xml $FEATUREXML;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $FEATUREXML;
 sed -i 's/theGroupID/'$GROUPID'/g' $FEATUREXML;
-cp features_TEMPLATE.pom $FEATURESPOM;
+mv -v features_TEMPLATE.pom $FEATURESPOM;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $FEATURESPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $FEATURESPOM;
 # .mvn/
-mkdir .mvn;
-cp extensions.xml .mvn/;
+mkdir -v .mvn;
+mv -v extensions.xml .mvn/;
 # .
-cp main_TEMPLATE.pom $MAINPOM;
+mv -v main_TEMPLATE.pom $MAINPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $MAINPOM;
 # releng/...aggregator
-mkdir -P $AGGREGATORFOLDER;
-cp aggregator_TEMPLATE.pom $AGGREGATORPOM;
+mkdir -v -p $AGGREGATORFOLDER;
+mv -v aggregator_TEMPLATE.pom $AGGREGATORPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $AGGREGATORPOM;
 # releng/...parent
-mkdir $PARENTFOLDER;
-cp parent_TEMPLATE.pom $PARENTPOM;
+mkdir -v $PARENTFOLDER;
+mv -v parent_TEMPLATE.pom $PARENTPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $PARENTPOM;
 # releng/...updatesite
-mkdir $UPDATESITEFOLDER;
-cp updatesite_TEMPLATE.project $UPDATESITEPROJECT;
+mkdir -v $UPDATESITEFOLDER;
+mv -v updatesite_TEMPLATE.project $UPDATESITEPROJECT;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITEPROJECT;
-cp category_TEMPLATE.xml $UPDATESITECATEGORY;
+mv -v category_TEMPLATE.xml $UPDATESITECATEGORY;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $UPDATESITECATEGORY;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITECATEGORY;
-cp updatesite_TEMPLATE.pom $UPDATESITEPOM;
+mv -v updatesite_TEMPLATE.pom $UPDATESITEPOM;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $UPDATESITEPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITEPOM;
 # releng/...updatesite
-mkdir $UPDATESITEAGGRFOLDER;
-cp updatesiteaggregated_TEMPLATE.project $UPDATESITEAGGRPROJECT;
+mkdir -v $UPDATESITEAGGRFOLDER;
+mv -v updatesiteaggregated_TEMPLATE.project $UPDATESITEAGGRPROJECT;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITEAGGRPROJECT;
-cp updatesiteaggregated_TEMPLATE.aggr $UPDATESITEAGGR;
+mv -v updatesiteaggregated_TEMPLATE.aggr $UPDATESITEAGGR;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $UPDATESITEAGGR;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITEAGGR;
-cp updatesiteaggregated_TEMPLATE.pom $UPDATESITEAGGRPOM;
+mv -v updatesiteaggregated_TEMPLATE.pom $UPDATESITEAGGRPOM;
 sed -i 's/thePlugInName/'$PLUGINNAME'/g' $UPDATESITEAGGRPOM;
 sed -i 's/theGroupID/'$GROUPID'/g' $UPDATESITEAGGRPOM;
-#####################################################################
+#########################################################################################
+echo "maventychonization done";
