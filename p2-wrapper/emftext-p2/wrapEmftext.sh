d778e4394640410f62e20c125b8fbf3267c2ce68
Travis-CI <travis-ci>
2018-12-18 15:53:02 +0100

    Promoting a new release build for p2-wrapper/emftext-p2

---
+++ p2-wrapper/emftext-p2/wrapEmftext.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# $1: name of the GitHub organization to deploy to
+# $2: name of the updatesite project on GitHub
+
+mvn clean verify
+export TRAVIS_PULL_REQUEST=false
+export TRAVIS_BRANCH=master
+export GITHUB_DEPLOY_TOKEN=git
+../../travis-ci/deploy.sh p2-wrapper/emftext-p2 . final "$1" "$2" 1.0.0
