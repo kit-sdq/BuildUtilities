49a56cb709a3034d64e9de3e6e6081dd3cd4d2b1
Travis-CI <travis-ci>
2018-12-18 17:40:59 +0100

    Enable artifact mirroring on EMFText udpatesite

--- p2-wrapper/emftext-p2/wrapEmftext.sh
+++ p2-wrapper/emftext-p2/wrapEmftext.sh
@@ -6,4 +6,4 @@ mvn clean verify
 export TRAVIS_PULL_REQUEST=false
 export TRAVIS_BRANCH=master
 export GITHUB_DEPLOY_TOKEN=git
-../../travis-ci/deploy.sh p2-wrapper/emftext-p2 . final "$1" "$2" 1.0.0
+../../travis-ci/deploy.sh p2-wrapper/emftext-p2 . final "$1" "$2"
