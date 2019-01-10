1a9976a25608a84b15f884066231790a051500d0
Travis-CI <travis-ci>
2019-01-10 13:58:04 +0100

    Enable deploying releases of p2-wrapper builds

--- p2-wrapper/emftext-p2/wrapEmftext.sh
+++ p2-wrapper/emftext-p2/wrapEmftext.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # $1: name of the GitHub organization to deploy to
 # $2: name of the updatesite project on GitHub
+# $3: release version to deploy or none if nightly deployment
+
+GITHUB_ORGANIZATION="$1"
+UPDATESITE_PROJECT="$2"
+EXPLICIT_RELEASE_VERSION=${3:-""}
 
 mvn clean verify
 export TRAVIS_PULL_REQUEST=false
 export TRAVIS_BRANCH=master
 export GITHUB_DEPLOY_TOKEN=git
-../../travis-ci/deploy.sh p2-wrapper/emftext-p2 . final "$1" "$2"
+../../travis-ci/deploy.sh p2-wrapper/emftext-p2 . final $GITHUB_ORGANIZATION $UPDATESITE_PROJECT $EXPLICIT_RELEASE_VERSION
