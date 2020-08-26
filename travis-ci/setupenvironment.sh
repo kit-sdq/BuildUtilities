e02b7b54367d687324b3ff67d69c696c6a8e351e
Heiko Klare <heiko.klare@kit.edu>
2020-08-26 16:54:12 +0200

    Remove Maven setup
    Current version used by Travis-CI works properly

--- travis-ci/setupenvironment.sh
+++ travis-ci/setupenvironment.sh
@@ -1,4 +1,7 @@
-wget https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.zip
-unzip -qq apache-maven-3.6.0-bin.zip
-export M2_HOME=$PWD/apache-maven-3.6.0
-export PATH=$M2_HOME/bin:$PATH
+# This script is supposed to set up the build environment at Travis-CI. It may install additional artifacts/tools or other versions of them, if necessary.
+
+# For example, Travis-CI used Maven versions that were not compatible with Eclipse plugins, pomless builds etc., which forced us to install a different Maven version as follows:
+# wget https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.zip
+# unzip -qq apache-maven-3.6.0-bin.zip
+# export M2_HOME=$PWD/apache-maven-3.6.0
+# export PATH=$M2_HOME/bin:$PATH
